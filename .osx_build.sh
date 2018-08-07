7df607aa0970cf74fb48aef34333c306eeb6a173
Jeff Zaremba <jzaremba@gmail.com>
2018-08-07 11:18:47 -0400

    Install numpy first.

--- .osx_build.sh
+++ .osx_build.sh
@@ -17,5 +17,8 @@ brew install hdf5
 if [[ "$TRAVIS_PYTHON_VERSION" == "py27" ]]; then
     sudo pip2 install numpy scipy matplotlib scikit-image shapely scikit-learn Pillow future toolz nose h5py Cython flake8 sphinx numpydoc picos
 else
-    sudo pip3 install numpy scipy matplotlib scikit-image shapely scikit-learn Pillow future toolz nose h5py Cython flake8 sphinx numpydoc picos
+	# Aug. 2018 - One of these packages is pulling in PyWavelets, which seems
+	# to require that numpy already be installed when building.
+	sudo pip3 install numpy
+    sudo pip3 install scipy matplotlib scikit-image shapely scikit-learn Pillow future toolz nose h5py Cython flake8 sphinx numpydoc picos
 fi
