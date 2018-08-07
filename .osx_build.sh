b4c42a67c5055136df028ebd96d6473a5163703d
Jeff Zaremba <jzaremba@gmail.com>
2018-08-07 11:57:36 -0400

    Pin OSX python3 to 3.5

--- .osx_build.sh
+++ .osx_build.sh
@@ -7,7 +7,12 @@ if [[ "$TRAVIS_PYTHON_VERSION" == "py35" ]]; then export PY_VERSION="python3"; f
 
 brew update
 brew outdated cmake || brew upgrade cmake
-brew outdated $PY_VERSION || brew upgrade $PY_VERSION
+if [[ "$PY_VERSION" == "python3" ]]; then
+    # Pin to Python3.5
+    brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/ec545d45d4512ace3570782283df4ecda6bb0044/Formula/python3.rb
+else
+    brew outdated $PY_VERSION || brew upgrade $PY_VERSION
+fi
 
 if [[ "$TRAVIS_PYTHON_VERSION" == "py35" ]]; then brew prune; fi
 if [[ "$TRAVIS_PYTHON_VERSION" == "py35" ]]; then brew linkapps python3; fi
@@ -17,7 +22,5 @@ brew install hdf5
 if [[ "$TRAVIS_PYTHON_VERSION" == "py27" ]]; then
     sudo pip2 install numpy scipy matplotlib scikit-image shapely scikit-learn Pillow future toolz nose h5py Cython flake8 sphinx numpydoc picos
 else
-    # Aug. 2018 - Some packages need to be build from source and require these.
-    sudo pip3 install Cython numpy
-    sudo pip3 install scipy matplotlib scikit-image shapely scikit-learn Pillow future toolz nose h5py flake8 sphinx numpydoc picos
+    sudo pip3 install numpy Cython scipy matplotlib scikit-image shapely scikit-learn Pillow future toolz nose h5py flake8 sphinx numpydoc picos
 fi
